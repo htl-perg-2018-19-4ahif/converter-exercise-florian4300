3dfc0a73758edcfd9fb19ec6cdf09c26cec2e266
Florian Hitzler <20150060@HTBLAPerg.onmicrosoft.com>
2018-10-19 12:39:34 +0200

    fixed a bug where mass to length was possible

--- convert.js
+++ convert.js
@@ -1,6 +1,6 @@
 let test = ["m", "dm", "cm", "mm"];
 let weight = ["kg","g"];
-if (process.argv.length != 6 || process.argv[4] != "to" || (test.indexOf(process.argv[5]) == -1 && weight.indexOf(process.argv[5]) == -1) || (test.indexOf(process.argv[3]) == -1 && weight.indexOf(process.argv[3]) == -1) || isNaN(process.argv[2])) {
+if (process.argv.length != 6 || process.argv[4] != "to" || (test.indexOf(process.argv[5]) == -1 && weight.indexOf(process.argv[5]) == -1) || (test.indexOf(process.argv[3]) == -1 && weight.indexOf(process.argv[3]) == -1) || isNaN(process.argv[2]) || (test.indexOf(process.argv[3]) != -1 && weight.indexOf(process.argv[5]) != -1) || (test.indexOf(process.argv[5]) != -1 && weight.indexOf(process.argv[3]) != -1 )) {
     console.log("Invalid parameters");
 } else if(test.indexOf(process.argv[5]) != -1 && test.indexOf(process.argv[3]) != -1){
     console.log(process.argv[2] + " " + process.argv[3] + " are "+(process.argv[2] /(Math.pow(10,test.indexOf(process.argv[3]) - test.indexOf(process.argv[5])))+ " " +process.argv[5]));

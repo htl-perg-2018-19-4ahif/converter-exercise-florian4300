5b40e9f916b284fb0bfd9b38a4836f504f6b94e3
Florian Hitzler <20150060@HTBLAPerg.onmicrosoft.com>
2018-10-19 12:13:51 +0200

    added first version of the converter

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet!');
+let test = ["m", "dm", "cm", "mm"];
+let weight = ["kg","g"];
+if (process.argv.length != 6 || process.argv[4] != "to" || (test.indexOf(process.argv[5]) == -1 && weight.indexOf(process.argv[5]) == -1) || (test.indexOf(process.argv[3]) == -1 && weight.indexOf(process.argv[3]) == -1) || isNaN(process.argv[2])) {
+    console.log("Invalid parameters");
+} else if(test.indexOf(process.argv[5]) != -1 && test.indexOf(process.argv[3]) != -1){
+    console.log(process.argv[2] + " " + process.argv[3] + " are "+(process.argv[2] /(Math.pow(10,test.indexOf(process.argv[3]) - test.indexOf(process.argv[5])))+ " " +process.argv[5]));
+}else{
+    console.log(process.argv[2] + " " + process.argv[3] + " are "+(process.argv[2] /(Math.pow(1000,weight.indexOf(process.argv[3]) - weight.indexOf(process.argv[5])))+ " " +process.argv[5]));
+}
